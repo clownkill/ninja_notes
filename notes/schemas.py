da8cdf2b4f75308529135f6a6b8dc1ee695a603d
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2025-03-04 18:15:25 +0300

    initial commit

---
+++ notes/schemas.py
@@ -0,0 +1,36 @@
+import datetime
+
+from ninja import Schema, ModelSchema
+from datetime import date
+
+from .models import Note
+
+
+class CategoryIn(Schema):
+    title: str
+    description: str
+
+
+class CategoryOut(Schema):
+    id: int
+    title: str
+    description: str
+    created: datetime.datetime
+
+
+class NoteIn(ModelSchema):
+    class Config:
+        model = Note
+        model_fields = ["title", "category"]
+
+
+class NoteUpd(ModelSchema):
+    class Config:
+        model = Note
+        model_fields = ["id", "completed"]
+
+
+class NoteOut(ModelSchema):
+    class Config:
+        model = Note
+        model_fields = ["id", "title", "category", "created", "completed"]
